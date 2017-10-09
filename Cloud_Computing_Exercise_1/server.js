466a843d24ec8f81bd8bc516c8f3c73228822296
JoachimZerbig <Joachim.Zerbig@Student.Reutlingen-University.DE>
2017-10-09 12:11:56 +0200

    Current state of project.

---
+++ Cloud_Computing_Exercise_1/server.js
@@ -0,0 +1,75 @@
+//https://www.youtube.com/watch?v=k8o8-Q_-Qfk&t=768s
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+users = {};
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('Server running...');
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+ 
+
+io.sockets.on('connection', function(socket){
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
+	//user ids for sending of private messages ==> where to place, what parameters etc????
+	console.log(socket.id);
+	
+	//Disconnect
+	socket.on('disconnect', function(data){
+		delete users[socket.nickname];
+		updateUsernames();
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected: %s sockets connected', connections.length);
+		io.sockets.emit('user left', {user: socket.nickname});
+	});
+	
+	//Send Message
+	socket.on('send message', function(data, callback){
+		var msg = data.trim();
+		if(msg.substring(0,3) == '/p '){
+			msg = msg.substring(3);
+			var i = msg.indexOf(' ');
+			if(i != -1){
+				var name = msg.substring(0, i);
+				var msg = msg.substring(i + 1);
+				if(name in users){
+					users[name].emit('private message', {msg: msg, user: socket.nickname, timestamp: new Date(Date.now()).toUTCString()});
+					
+					console.log('private message');
+				} else {
+					callback('There is no user with such name. Please try again.');
+				}
+			} else {
+				callback('The private message cannot be empty');
+			}
+			
+		}else{
+			io.sockets.emit('new message', {msg: msg, user: socket.nickname, timestamp: new Date(Date.now()).toUTCString()});
+		}
+	});
+	
+	//New User
+	socket.on('new user', function(data, callback){
+		if(data in users){
+			callback(false);
+		} else {
+			callback(true);
+			socket.nickname = data;
+			users[socket.nickname] = socket;
+			updateUsernames();
+			io.sockets.emit('new user enlisted', {user: socket.nickname});
+		}
+	});
+	
+	function updateUsernames(){
+		io.sockets.emit('get users', Object.keys(users));
+	}
+	
+});
