3c630c5ddbc20ab275ce9db39dd495e2893bfed3
JoachimZerbig <Joachim.Zerbig@Student.Reutlingen-University.DE>
2017-11-17 09:48:24 +0100

    hallo test

--- Cloud_Computing_Exercise_1/server.js
+++ Cloud_Computing_Exercise_1/server.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-var portNumber = 3000;
+var portNumber = process.env.PORT || 3000;
 
 
 users = {};
